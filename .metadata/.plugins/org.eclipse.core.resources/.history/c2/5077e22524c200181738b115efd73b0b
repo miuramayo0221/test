cd361e8dc53c0d9b0763fb4d1b93d9cfcf33dd06
miuramayo <wwp.p.stk363158@gmail.com>
2018-09-28 10:26:41 +0900

    test version01

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c2/5077e22524c200181738b115efd73b0b
@@ -0,0 +1,19 @@
+public class Android{
+ public static void main(String[] args){
+  SmartPhone1 Android = new SmartPhone1();
+
+  Android.play();
+
+  Android.stop();
+
+  Android.back();
+
+  Android.call();
+
+  Android.mail();
+
+  Android.photo();
+
+  Android.internet();
+ }
+}
---
+++ インスタンス化_Person/src/Test1.java
@@ -0,0 +1,21 @@
+public class Test1 {
+ public static void main(String[]args){
+	 Person ziro =new Person();
+	 ziro.name="木村次郎";
+	 ziro.age=20;
+	 System.out.println(ziro.name);
+	 System.out.println(ziro.age);
+
+	 Person hanako =new Person();
+	 hanako.name="鈴木花子";
+	 hanako.age=16;
+	 System.out.println(hanako.name);
+	 System.out.println(hanako.age);
+
+	 Person mayo =new Person();
+	 mayo.name="三浦真葉";
+	 mayo.age=20;
+	 System.out.println(mayo.name);
+	 System.out.println(mayo.age);
+ }
+}
